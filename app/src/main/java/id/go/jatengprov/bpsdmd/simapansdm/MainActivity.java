afd95559dd1f17bef60d52cd27c9616a931c4a9d
Donny Malik Kurniawan <bossdony@gmail.com>
2021-05-24 15:31:27 +0700

    Beres
    What's new:
    - Upload file
    - Download file
    - Swipe to refresh page

--- app/src/main/java/id/go/jatengprov/bpsdmd/simapansdm/MainActivity.java
+++ app/src/main/java/id/go/jatengprov/bpsdmd/simapansdm/MainActivity.java
@@ -2,36 +2,198 @@ package id.go.jatengprov.bpsdmd.simapansdm;
 
 import android.app.Activity;
 //import android.support.v7.app.AppCompatActivity;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
 import android.content.Intent;
 import android.net.Uri;
-import android.view.KeyEvent;
+//import android.view.KeyEvent;
+import android.widget.Toast;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 
 public class MainActivity extends Activity {
     private WebView myWebView = null;
+    private SwipeRefreshLayout swipe;
+
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // alamat websitenya
+        String alamatUtama = "https://bpsdmd.jatengprov.go.id/dashboard/pns/Dashboard";
+        String getAlamat = "";
+
+        // progres dialog ketika memuat halaman
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Memuat data...");
+        progressDialog.setCancelable(true);
+
+        // action ketika layar ditarik ke bawah (refresh)
+        swipe = findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                LoadWeb(getAlamat);
+            }
+        });
+
+        LoadWeb(alamatUtama);
+    }
+
+    public void LoadWeb(String url){
         myWebView = findViewById(R.id.web_view);
         //enable Javascript
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
 
         myWebView.setWebViewClient(new WebViewClient());
         //myWebView.setWebViewClient(new MyWebViewClient());
-        myWebView.loadUrl("https://bpsdmd.jatengprov.go.id/dashboard/pns/Dashboard");
+
+        if(url.equals("")){
+            url = myWebView.getUrl();
+        }
+        myWebView.loadUrl(url);
+
+        // konfigurasi Chrome client untuk progress dialog dan upload file
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+/*                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }*/
+
+                //loading akan jalan lagi ketika masuk link lain
+                // dan akan berhenti saat loading selesai
+                if(myWebView.getProgress()== 100){
+                    swipe.setRefreshing(false);
+                } else {
+                    swipe.setRefreshing(true);
+                }
+            }
+
+            // For 3.0+ Devices (Start)
+            // onActivityResult attached before constructor
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    intent = fileChooserParams.createIntent();
+                }
+                try {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e) {
+                    uploadMessage = null;
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+
+        });
+
+        // action ketika mendownload file
+        myWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription("Mengunduh berkas...");
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Mengunduh berkas...", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    // Memproses hasil unggahan
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+        else
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Gagal mengunggah berkas", Toast.LENGTH_LONG).show();
+    }
+
+    // jika tombol back pada android di-klik, kembali ke halaman web sebelumnya (jika bisa)
     @Override
     public void onBackPressed() {
-        // jika tombol back pada android di-klik kembali ke halaman web sebelumnya (jika bisa)
         if(myWebView.canGoBack()){
             myWebView.goBack();
         }
@@ -40,31 +202,5 @@ public class MainActivity extends Activity {
         }
     }
 
-    /*    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // Check if the key event was the Back button and if there's history
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
-            myWebView.goBack();
-            return true;
-        }
-        // If it wasn't the Back key or there's no web page history, bubble up to the default
-        // system behavior (probably exit the activity)
-        return super.onKeyDown(keyCode, event);
-    }
 
-
-
-    private class MyWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals("https://bpsdmd.jatengprov.go.id/")) {
-                // This is my website, so do not override; let my WebView load the page
-                return false;
-            }
-            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-            return true;
-        }
-    }*/
 }
